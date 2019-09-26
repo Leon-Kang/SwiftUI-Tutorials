d9ae022979717f55c35bd6ad62023423cc46c29c
Leon-Kang <coderleonkang@gmail.com>
2019-09-26 15:19:29 +0800

    Initial Commit

---
+++ 44584/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  44584
+//
+//  Created by Leon Kang on 2019/9/26.
+//  Copyright © 2019 Leon Kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
