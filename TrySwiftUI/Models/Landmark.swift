cb3812a0f7e51498dc487f3c17aa441048acd8cc
Leon-Kang <coderleonkang@gmail.com>
2019-09-26 17:46:47 +0800

    starting navigation and list point

---
+++ TrySwiftUI/Models/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  TrySwiftUI
+//
+//  Created by Leon Kang on 2019/9/26.
+//  Copyright © 2019 Leon Kang. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
