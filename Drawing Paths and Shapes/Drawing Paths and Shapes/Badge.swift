8a6eb1cefb024de21f9e7cf2ae17e02fd4420864
Leon-Kang <coderleonkang@gmail.com>
2019-09-27 15:34:18 +0800

    Drawing badge shape

--- Drawing Paths and Shapes/Drawing Paths and Shapes/Badge.swift
+++ Drawing Paths and Shapes/Drawing Paths and Shapes/Badge.swift
@@ -10,8 +10,53 @@ import SwiftUI
 
 struct Badge: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                
+                path.move(to:
+                    CGPoint(
+                        x: xOffset + width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.points.forEach { (segment) in
+                    path.addLine(
+                        to: .init(
+                            x: xOffset + width * segment.useWidth.0 * segment.xFactors.0,
+                            y: height * segment.useHeight.0 * segment.yFactors.0
+                        )
+                    )
+                
+                    path.addQuadCurve(
+                        to: .init(
+                            x: xOffset + width * segment.useWidth.1 * segment.xFactors.1,
+                            y: height * segment.useHeight.1 * segment.yFactors.1
+                        ),
+                        control: .init(
+                            x: xOffset + width * segment.useWidth.2 * segment.xFactors.2,
+                            y: height * segment.useHeight.2 * segment.yFactors.2
+                        )
+                    )
+                }
+            }
+            .fill(LinearGradient(
+                gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: .init(x: 0.5, y: 0),
+                endPoint: .init(x: 0.5, y: 0.6)
+            ))
+            .aspectRatio(1, contentMode: .fit)
+        }
     }
+    
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+
 }
 
 struct Badge_Previews: PreviewProvider {
